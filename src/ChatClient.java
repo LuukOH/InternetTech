c4bbe8a28a54cfd7459c802dc5a43f3f4bba5ca1
Unknown <damianjamesjansen@gmail.com>
2017-11-23 13:58:25 +0100

    output is done

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,6 +1,26 @@
+import java.io.IOException;
+import java.net.Socket;
+
 public class ChatClient {
+    Socket connection;
 
     public static void main(String[] args) {
-        
+        try {
+            ChatClient chatClient = new ChatClient();
+            Socket connection = chatClient.makeConnection();
+
+            InputThread inputThread = new InputThread(connection);
+            OutputThread outputThread = new OutputThread(connection);
+            inputThread.start();
+            outputThread.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    public Socket makeConnection() throws IOException{
+        connection = new Socket("localhost",1337);
+        return connection;
+    }
+
 }
--- src/InputThread.java
+++ src/InputThread.java
@@ -1,2 +1,24 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+
 public class InputThread extends Thread {
+    Socket connection;
+
+    public InputThread(Socket connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public void run() {
+        InputStream inputStream = null;
+        try {
+            inputStream = connection.getInputStream();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
