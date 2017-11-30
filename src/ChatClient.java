8e209799a04dd82d30eccf64baf36b7c184b6391
Unknown <damianjamesjansen@gmail.com>
2017-11-30 15:47:41 +0100

    added reconnect and dropped packacges

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,25 +1,57 @@
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ChatClient {
     Socket connection;
+    public static String GOOD = "GOOD";
+    public static String RETRY = "RETRY";
+    public String status = RETRY;
 
     public static void main(String[] args) {
         try {
             ChatClient chatClient = new ChatClient();
-            Socket connection = chatClient.makeConnection();
-
-            InputThread inputThread = new InputThread(connection);
-            OutputThread outputThread = new OutputThread(connection);
-            inputThread.start();
-            outputThread.start();
+            chatClient.run();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public Socket makeConnection() throws IOException{
-        connection = new Socket("localhost",1337);
+    public void run() throws IOException {
+        while (status.equals(RETRY)){
+            status = GOOD;
+            Socket connection = makeConnection();
+            CopyOnWriteArrayList<String> messageList = new CopyOnWriteArrayList<>();
+            InputThread inputThread = new InputThread(connection, messageList,this);
+            inputThread.start();
+
+            OutputStream outputStream;
+            try {
+                outputStream = connection.getOutputStream();
+                String input = "";
+                Scanner scanner = new Scanner(System.in);
+                PrintWriter writer = new PrintWriter(outputStream);
+
+                while (!input.equals("QUIT") && inputThread.isAlive()){
+                    input = scanner.nextLine();
+                    if (!input.equals("")){
+                        messageList.add(input);
+                        writer.println(input);
+                        writer.flush();
+                    }
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public Socket makeConnection() throws IOException {
+        connection = new Socket("localhost", 1337);
         return connection;
     }
 
--- src/InputThread.java
+++ src/InputThread.java
@@ -1,13 +1,24 @@
 import java.io.*;
 import java.net.Socket;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 
 public class InputThread extends Thread {
+    public enum ServerMessage {
+        OK,BCST,ERR,UNKNOWN,HELO;
+    }
     Socket connection;
+    CopyOnWriteArrayList messageList;
+    private ChatClient chatClient;
     boolean quit = false;
+    boolean noResponse = false;
+    boolean dropRetry = false;
+    long time;
 
-    public InputThread(Socket connection) {
+    public InputThread(Socket connection,CopyOnWriteArrayList copyOnWriteArrayList,ChatClient chatClient) {
         this.connection = connection;
+        messageList = copyOnWriteArrayList;
+        this.chatClient = chatClient;
     }
 
     @Override
@@ -15,15 +26,41 @@ public class InputThread extends Thread {
         InputStream inputStream;
         try {
             inputStream = connection.getInputStream();
-            String line = "";
+            String line;
             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
             while (!quit){
-                if ((line = reader.readLine()) != null){
+                if (reader.ready()){
+                    line = reader.readLine();
+                    if (messageList.size() > 0){
+                        messageList.remove(messageList.size() - 1);
+                        dropRetry = false;
+                        noResponse = false;
+                    }
                     if (line.equals("+OK Goodbye")){
                         quit = true;
                     }
                     System.out.println(line);
+                } else if (messageList.size() > 0){
+                    if (noResponse){
+                        long timeTaken = System.currentTimeMillis() - time;
+                        if (timeTaken > 1000 && timeTaken < 10000){
+                            if (!dropRetry) {
+                                PrintWriter writer = new PrintWriter(connection.getOutputStream());
+                                writer.println(messageList.get(messageList.size() - 1));
+                                writer.flush();
+                                dropRetry = true;
+                            }
+                        } else if (timeTaken > 10000){
+                            time = System.currentTimeMillis();
+                            System.out.println("connection failed! trying to reconnect. Type anything to reconnect");
+                            chatClient.status = ChatClient.RETRY;
+                            quit = true;
+                        }
+                    } else {
+                        time = System.currentTimeMillis();
+                        noResponse = true;
+                    }
                 }
             }
             connection.close();
